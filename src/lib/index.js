b08459c9e86685db103ce79a5da87f27d7df1881
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-06-07 17:35:47 -0700

    doc

--- src/lib/index.js
+++
@@ -1,8 +0,0 @@
-import createAutoSizer from './components/createAutoSizer'
-
-/**
- * react-autosizer
- * Takes object with dependencies (React) and exports a component that controls a target(s) heighth/widths, based on a source height / width.
- * @return {Object}              <AutoSizer /> Component Factory
- */
-export default createAutoSizer
